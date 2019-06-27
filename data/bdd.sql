39c34d46b3b4b9c7f71d9f865dddded87fadf41c
AbrahamBriones <abraham.briones1501@alumnos.ubiobio.cl>
2019-06-27 11:47:30 -0400

    Create BDD

---
+++ data/bdd.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE yaprofe;
+
+CREATE TABLE modalidad (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(20)
+);
+
+CREATE TABLE niveleducacional (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(60)
+);
+
+CREATE TABLE asignatura (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(60)
+);
+
+CREATE TABLE users (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(60) NOT NULL,
+	lastname VARCHAR(120)NOT NULL,
+	email VARCHAR(250) NOT NULL,
+	password VARCHAR(250)NOT NULL,
+	ciudad VARCHAR(60),
+	telefono VARCHAR(30),
+	descripcion VARCHAR(500),
+    id_modalidad INTEGER,
+    id_niveleducacional INTEGER,
+    id_asignatura INTEGER,
+	INDEX (id_modalidad),
+    INDEX (id_niveleducacional),
+    INDEX (id_asignatura),
+    FOREIGN KEY (id_modalidad) REFERENCES modalidad(id),
+	FOREIGN KEY (id_niveleducacional) REFERENCES niveleducacional(id),
+	FOREIGN KEY (id_asignatura) REFERENCES asignatura(id)
+);
+
+
+
